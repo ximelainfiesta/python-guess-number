f194d9036e5b35f2eb3d4e96193fd082dc8ec11b
ximelainfiesta <ximena.lainfiesta@gmail.com>
2015-02-05 14:33:11 -0600

    Changed everything to English and made the first stories verifying the guess of the user

--- application.py
+++ application.py
@@ -1,13 +1,14 @@
-import random #Se importa la función random
+import random #Imports the random function
 
-aleatorio = random.randint(1,20) #genera un número aleatorio de 1 a 20
-guess = input("Welcome, guess the number: ") #Le pide al usuario que ingrese un número
+aleatorio = random.randint(1,20) #generates a random number from 1 to 20
+guess = input("Welcome, guess the number: ") #asks the user for a number
 
-if guess > aleatorio: #compara los valores del usuario y el aleatorio y asume que la respuesta es mayor
+#This next if statement helps me compare the variables with the <> and == symbols
+if guess > aleatorio: #this compare the guess as higher than the random number
 	print "you guessed to high, please try again"
-elif guess < aleatorio: #compara la respuesta del usuario y el aleatorio, asumiendo que la del usuario es menor
+elif guess < aleatorio: #this compare the guess as lower than the random number
 	print "you guessed to low, please try again"
-elif guess == aleatorio: #compara ambos valores dados, y asume que ambos son el mismo número
-	print "you win"
+elif guess == aleatorio: #this compare the guess as equal as the random number
+	print "you win!"
 
-print aleatorio #me sirve para verificar que el aleatorio efectivamente es menor o mayor (este es temporal)
+print aleatorio #it helps me to verify that the machine is working and sending the correct answer according to the guess
