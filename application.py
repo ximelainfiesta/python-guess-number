d678179c38c8f615860df8278a1ee232e86503ba
ximelainfiesta <ximena.lainfiesta@gmail.com>
2015-02-06 14:47:52 -0600

    Game finished correctly with everything, changes in english are needed

--- application.py
+++ application.py
@@ -1,19 +1,66 @@
 import random #Imports the random function
 
-count = 0 #it helps the while statement to start the count in 0
+newgame = True
 
-while count < 4: #it makes the loop go until 4
-	aleatorio = random.randint(1,20) #generates a random number from 1 to 20
-	guess = input("Welcome, guess the number: ") #asks the user for a number
-#This next if statement helps me compare the variables with the <> and == symbols
-	if guess > aleatorio: #this compare the guess as higher than the random number
-		print "you guessed to high, please try again"
-	elif guess < aleatorio: #this compare the guess as lower than the random number
-		print "you guessed to low, please try again"
-	elif guess == aleatorio: #this compare the guess as equal as the random number
-		print "you win!"
-	count += 1 #it adds a count every time the loops goes on so it doesnt keep going eternally
-print "Game Over"
+while newgame == True: #it makes the loop go until 4
+	valeatory = random.randint(1,20)
+	print valeatory
+	count = -1 #it helps the while statement to start the count in 0
+	
+	validar = True
+	while validar == True:
+		count += 1 #it adds a count every time the loops goes on so it doesnt keep going eternally
+		if count < 4: 	
+			try:
+				guess = input("Welcome, guess the number: ") #asks the user for a number
+				if guess == valeatory: #this compare the guess as equal as the random number
+					print "You win!"
+					respuesta = False
+					while respuesta == False:
+						print "Do you want to play again? y/n: "
+						jugardenuevo = raw_input('> ')
+						jugardenuevo = jugardenuevo.lower()
+						if jugardenuevo == "y":
+							respuesta = True
+							validar = False
+						elif jugardenuevo == "n":
+							respuesta = True
+							validar = False
+							newgame = False
+							print "Good - Bye"
+						else:
+							print "Invalid Option"
 
-print aleatorio #it helps me to verify that the machine is working and sending the correct answer according to the guess
+					#This next if statement helps me compare the variables with the <> and == symbols
+				elif guess > valeatory: #this compare the guess as higher than the random number
+					print "You guessed to high, please try again"
+				elif guess < valeatory: #this compare the guess as lower than the random number
+					print "You guessed to low, please try again"
+		
+				else: 
+					pass
+
+			except NameError:
+				print "Enter only numbers"
+			except SyntaxError:
+				print "Enter only numbers"
+		else:
+			print "Game Over!"
+			respuesta = False
+			while respuesta == False:
+				print "Do you want to play again? y/n: "
+				jugardenuevo = raw_input('> ')
+				jugardenuevo = jugardenuevo.lower()
+				if jugardenuevo == "y":
+					respuesta = True
+					validar = False
+				elif jugardenuevo == "n":
+					respuesta = True
+					validar = False
+					newgame = False
+					print "Good - Bye"
+				else:
+					print "Invalid Option"
+
+
 
